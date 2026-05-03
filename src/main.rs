3827aeeb36aef259aff71e3aacbaa5a41d21fe80
bitsixfour <tugirasles@gmail.com>
2026-05-03 16:12:37 -0500

    new

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ use std::fs;
 use std::path::Path;
 use time::OffsetDateTime;
 use anyhow::{Context, Result};
+use csv::Reader;
 
 mod sql;
 use sql::import_csv;
@@ -13,9 +14,15 @@ use sql::import_csv;
 
 #[tokio::main]
 async fn main() -> Result<(), anyhow::Error>   {
+    /* may be depricated */
     println!("Importing CSV data... \n
         I read /obsidianfm/data/*");
     let mut conn = Connection::open("music.db")?;
     sql::import_csv(&mut conn);
+
+    let mut album = csv::Reader::from_reader(io::stdin());
+    for result in album.records() {
+        println!("test");
+    }
     Ok(())
 }
